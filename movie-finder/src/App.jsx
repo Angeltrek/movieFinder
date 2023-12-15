68703925621923ad263ae33e083f6fa243695631
AngelMauricioRamirez <A01710158@tec.mx>
2023-12-14 20:17:35 -0600

    Add responsive grid

--- movie-finder/src/App.jsx
+++ movie-finder/src/App.jsx
@@ -1,20 +1,53 @@
 import './App.css'
 import { useMovies } from './hooks/useMovies';
 import {Movies} from './components/movies'
+import {useEffect, useState, useRef} from 'react'
 // useRef is a hook used to create a mutable object 
 // that persists across renders of a functional component
-import { useRef } from 'react'
+
+function useSearch() {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if(isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if(search === '') {
+      setError('Can not search an empty input')
+      return
+    }
+
+    if(search.match(/^\d+$/)) {
+      setError("Can not search only numbers")
+      return
+    }
+
+    if(search.length < 4) {
+      setError("The search needs more than 3 characters")
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return {search, updateSearch, error}
+}
 
 function App() {
-
   const {movies} = useMovies();
-  const inputRef = useRef();
+  const {search, updateSearch, error} = useSearch()
 
   const handleSumbit = (event) => {
     event.preventDefault()
-    const inputEl = inputRef.current
-    const value = inputEl.value
-    console.log(value)
+    console.log({search})
+  }
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
   }
 
   return (
@@ -23,9 +56,12 @@ function App() {
       <header>
         <h1>Movie Finder</h1>
         <form className='form' onSubmit={handleSumbit}>
-          <input ref={inputRef} placeholder='Avengers, Toy Story, Star Wars...' />
+          <input style={{border: '1px solid transparent', 
+          borderColor: error? 'red':'transparent'}} 
+          onChange={handleChange} name='query' placeholder='Avengers, Toy Story, Star Wars...' />
           <button type='submit'>Search</button>
         </form>
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </header>
 
       <main>
