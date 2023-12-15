99d5e409a7f60f2c54459816e56884cff4169a3d
AngelMauricioRamirez <A01710158@tec.mx>
2023-12-14 18:51:46 -0600

    Modified App.jsx

--- movie-finder/src/App.jsx
+++ movie-finder/src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
   return (
     <div className='page'>
       <header>
@@ -12,7 +16,25 @@ function App() {
       </header>
 
       <main>
-        Aqui irán los resultados
+        {
+          hasMovies
+          ?(
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title} />
+                  </li>
+                ))
+              }
+            </ul>
+          )
+          : (
+            <p>We have no results for your search</p>
+          )
+        }
       </main>
     </div>
   )
