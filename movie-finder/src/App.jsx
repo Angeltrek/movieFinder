7361efb3ec341134723733b52e4d70727e5ba87e
AngelMauricioRamirez <A01710158@tec.mx>
2023-12-14 19:26:37 -0600

    Modified App.jsx

--- movie-finder/src/App.jsx
+++ movie-finder/src/App.jsx
@@ -1,18 +1,29 @@
 import './App.css'
 import { useMovies } from './hooks/useMovies';
 import {Movies} from './components/movies'
+// useRef is a hook used to create a mutable object 
+// that persists across renders of a functional component
+import { useRef } from 'react'
 
 function App() {
 
   const {movies} = useMovies();
+  const inputRef = useRef();
+
+  const handleSumbit = (event) => {
+    event.preventDefault()
+    const inputEl = inputRef.current
+    const value = inputEl.value
+    console.log(value)
+  }
 
   return (
     <div className='page'>
 
       <header>
         <h1>Movie Finder</h1>
-        <form className='form'>
-          <input placeholder='Avengers, Toy Story, Star Wars...' />
+        <form className='form' onSubmit={handleSumbit}>
+          <input ref={inputRef} placeholder='Avengers, Toy Story, Star Wars...' />
           <button type='submit'>Search</button>
         </form>
       </header>
